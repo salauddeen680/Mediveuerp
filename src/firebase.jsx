b494a3186c943e8572ca9842b142c58e6548ed0a
salauddeen680 <salauddeen680@gmail.com>
2026-07-28 00:21:45 +0530

    Refactor Firebase config to use Vite env variables
    Updated Firebase configuration to use Vite environment variables for better security and flexibility.

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,14 +5,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; // Database ke liye
 import { getAuth } from "firebase/auth"; // Login ke liye
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using Vite Environment Variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDjmAHxhhEmIfpmMC-glskeSHNXuVXXlBU",
-  authDomain: "mediveuerp.firebaseapp.com",
-  projectId: "mediveuerp",
-  storageBucket: "mediveuerp.firebasestorage.app",
-  messagingSenderId: "203997741439",
-  appId: "1:203997741439:web:a44659832e6a107d80da4a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -21,4 +21,3 @@ const app = initializeApp(firebaseConfig);
 // App ke baaki hisso mein use karne ke liye inko export karein
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
