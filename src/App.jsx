c1ec2280e8a4e88113f750abd6fe55d6c0a27d32
salauddeen680 <salauddeen680@gmail.com>
2026-07-28 00:24:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,24 @@ import {
 } from 'firebase/firestore';
 
 // --- FIREBASE SETUP ---
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDjmAHxhhEmIfpmMC-glskeSHNXuVXXlBU",
-  authDomain: "mediveuerp.firebaseapp.com",
-  projectId: "mediveuerp",
-  storageBucket: "mediveuerp.firebasestorage.app",
-  messagingSenderId: "203997741439",
-  appId: "1:203997741439:web:a44659832e6a107d80da4a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
 
 // --- UTILITY COMPONENTS ---
 
